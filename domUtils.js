af5f2e3614296b5f07c89347f7e9d918aa087fee
Nicholas Hoyt <nhoyt@illinois.edu>
2021-04-22 15:16:27 -0500

    Initial commit

---
+++ domUtils.js
@@ -0,0 +1,101 @@
+/* domUtils.js */
+
+function getAccessibleName (landmark) {
+  const labelledbyIds = landmark.getAttribute('aria-labelledby'),
+    label = landmark.getAttribute('aria-label'),
+    title = landmark.getAttribute('title');
+
+  if (labelledbyIds && labelledbyIds.length) {
+    let ids = labelledbyIds.split(' '), strings = [];
+    if (!ids.length) ids = [labelledbyIds]; // is this necessary?
+
+    ids.forEach(id => {
+      let elem = document.getElementById(id);
+      if (elem) {
+        let text = getTextContent(elem);
+        if (text & text.length) {
+          strings.push(text);
+        }
+      }
+    });
+    return strings.join(' ');
+  }
+
+  if (isNotEmptyString(label)) {
+    return label;
+  }
+
+  if (isNotEmptyString(title)) {
+    return title;
+  }
+
+  return '';
+}
+
+/*
+**  getTextContent: called by getAccessibleName
+*/
+function getTextContent (elem) {
+
+  function getText (e, strings) {
+    // If text node, get the text and return
+    if (e.nodeType === Node.TEXT_NODE) {
+      strings.push(e.data);
+    }
+    else {
+      // If element node, traverse all child elements looking for text
+      if (e.nodeType === Node.ELEMENT_NODE) {
+        // If IMG or AREA element, use ALT content if defined
+        let tagName = e.tagName.toLowerCase();
+        if (tagName === 'img' || tagName === 'area') {
+          if (e.alt) {
+            strings.push(e.alt);
+          }
+        }
+        else {
+          let c = e.firstChild;
+          while (c) {
+            getText(c, strings);
+            c = c.nextSibling;
+          } // end loop
+        }
+      }
+    }
+  } // end getStrings
+
+  // Create return object
+  let str = 'Test',
+    strings = [];
+  getText(elem, strings);
+  if (strings.length) {
+    str = strings.join(' ');
+  }
+  return str;
+}
+
+function isNotEmptyString (str) {
+  return typeof str === 'string' && str.length;
+}
+
+function isEmptyString (str) {
+  return typeof str !== 'string' || str.length === 0;
+}
+
+function isVisible (element) {
+  function isVisibleRec (el) {
+    if (el.nodeType === Node.DOCUMENT_NODE) return true;
+
+    let computedStyle = window.getComputedStyle(el, null);
+    let display = computedStyle.getPropertyValue('display');
+    let visibility = computedStyle.getPropertyValue('visibility');
+    let hidden = el.getAttribute('hidden');
+    let ariaHidden = el.getAttribute('aria-hidden');
+
+    if ((display === 'none') || (visibility === 'hidden') ||
+        (hidden !== null) || (ariaHidden === 'true')) {
+      return false;
+    }
+    return isVisibleRec(el.parentNode);
+  }
+  return isVisibleRec(element);
+}
