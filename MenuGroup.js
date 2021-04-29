52116e0206ff1c94e0451048c0049c1edce16f11
Nicholas Hoyt <nhoyt@illinois.edu>
2021-04-28 21:24:54 -0500

    Update MenuGroup.js: add stylesheet to group

--- MenuGroup.js
+++ MenuGroup.js
@@ -10,6 +10,12 @@ class LandmarksGroup extends HTMLElement {
   constructor () {
     super();
     this.attachShadow({ mode: 'open' });
+
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', 'menu.css');
+    this.shadowRoot.appendChild(link);
+
     this.shadowRoot.appendChild(landmarksGrp.content.cloneNode(true));
     this.onMenuitemClicked = defaultClickHandler;
   }
@@ -71,6 +77,12 @@ class HeadingsGroup extends HTMLElement {
   constructor () {
     super();
     this.attachShadow({ mode: 'open' });
+
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', 'menu.css');
+    this.shadowRoot.appendChild(link);
+
     this.shadowRoot.appendChild(headingsGrp.content.cloneNode(true));
     this.onMenuitemClicked = defaultClickHandler;
   }
@@ -91,13 +103,15 @@ class HeadingsGroup extends HTMLElement {
     const group = this.shadowRoot.querySelector('div[role="group"]');
     const emptyContentMsg = '[empty text content]';
     for (const info of headingsInfo) {
-      const div = this.createMenuitem('headings', info.dataId);
+      const div = this.createMenuitem('heading', info.dataId);
+      if (info.tagName === 'h1') { div.classList.add('h1') }
 
       const a = document.createElement('a');
       a.href = '#';
 
       const textSpan = document.createElement('span');
       textSpan.className = 'text';
+      textSpan.classList.add(info.tagName);
       textSpan.textContent = info.content ? info.content : emptyContentMsg;
       a.appendChild(textSpan);
 
