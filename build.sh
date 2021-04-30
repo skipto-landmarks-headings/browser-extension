184a2d6a0cdac2d97b9976d2eac93e155f7ad07a
Nicholas Hoyt <nhoyt@illinois.edu>
2021-04-30 12:46:14 -0500

    Consistent widths and font-size for Chrome and Firefox

--- build.sh
+++ build.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
 GPPFILES=(
-  content
-  popup
+  content.js
+  menu.css
+  popup.js
 )
 
 # Process extension JS files: output to browser folders
 
 for FNAME in ${GPPFILES[@]}
 do
-  gpp -DCHROME=1 -o chrome/${FNAME}.js gpp-${FNAME}.js
+  gpp -DCHROME=1 -o chrome/${FNAME} gpp-${FNAME}
 done
 
 for FNAME in ${GPPFILES[@]}
 do
-  gpp -DFIREFOX=1 -o firefox/${FNAME}.js gpp-${FNAME}.js
+  gpp -DFIREFOX=1 -o firefox/${FNAME} gpp-${FNAME}
 done
-
-# Process manifest.json
-# gpp -o chrome/manifest.json manifest.json
-# gpp -DFIREFOX=1 -o firefox/manifest.json manifest.json
 
 # Copy shared extension files to browser folders
 
